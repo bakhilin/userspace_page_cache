3760e6eedef764dc5adbb031607188483b552c36
Bakhilin Nikita <bakhilinnikita04@mail.ru>
2024-12-08 23:58:29 +0300

    first push in main

---
+++ include/cache.h
@@ -0,0 +1,22 @@
+#ifndef CACHE_H
+#define CACHE_H
+
+#include <stddef.h>
+#include <sys/types.h>
+
+typedef struct cache_write{
+    int fd;
+    int valid;
+    int referenced; // second chance
+    off_t offset;
+    void* data;
+} cache_write_t;
+
+int lab2_open(const char * path);
+int lab2_close(int fd);
+ssize_t lab2_read(int fd, void* buf, size_t count);
+ssize_t lab2_write(int fd,  void * buf, size_t count);
+off_t lab2_lseek(int fd, off_t offset, int whence);
+int lab2_fsync(int fd);
+
+#endif
