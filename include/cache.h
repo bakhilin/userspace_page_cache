a9f7ee675aba0a6ab19da6c60e9acd5d1023eb07
Bakhilin Nikita <bakhilinnikita04@mail.ru>
2024-12-12 22:07:39 +0300

    add test

--- include/cache.h
+++ include/cache.h
@@ -4,6 +4,9 @@
 #include <stddef.h>
 #include <sys/types.h>
 
+#define CACHE_SIZE 10
+#define BLOCK_SIZE 4096
+
 typedef struct cache_write{
     int fd;
     int valid;
@@ -18,5 +21,5 @@ ssize_t lab2_read(int fd, void* buf, size_t count);
 ssize_t lab2_write(int fd,  void * buf, size_t count);
 off_t lab2_lseek(int fd, off_t offset, int whence);
 int lab2_fsync(int fd);
-
+cache_write_t * search_in_cache_mem(int fd, off_t offset);
 #endif
