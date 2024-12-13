31b5be04a350ed350e88e0bdb7b4d9738ef074e8
Bakhilin Nikita <bahilinnikita04@mail.ru>
2024-12-13 09:56:29 +0300

    d

---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include "../include/cache.h"
+#include <CUnit/CUnit.h>
+#include <CUnit/Basic.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define FILE_NAME "data.txt"
