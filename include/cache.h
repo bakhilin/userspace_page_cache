89b857b0efaf9f4b8122708ee65854b5e860d0ea
Bakhilin Nikita <bahilinnikita04@mail.ru>
2024-12-13 10:29:34 +0300

    d

--- include/cache.h
+++ include/cache.h
@@ -1,5 +1,4 @@
 #ifndef CACHE_H
-
 #define CACHE_H
 
 #include <stddef.h>
@@ -8,20 +7,19 @@
 #define CACHE_SIZE 10
 #define BLOCK_SIZE 4096
 
-typedef struct cache_write1{
-    int fd1;
-    int valid1;
-    int referenced1; // second chance
-    off_t offset1;
-    void* data1;
-} cache_write_t1;
+typedef struct cache_write{
+    int fd;
+    int valid;
+    int referenced; // second chance
+    off_t offset;
+    void* data;
+} cache_write_t;
 
-int lab1_open(const char * path);
-int lab1_close(int fd);
-ssize_t lab1_read(int fd, void* buf, size_t count);
-ssize_t lab1_write(int fd,  void * buf, size_t count);
-off_t lab1_lseek(int fd, off_t offset, int whence);
-int lab1_fsync(int fd);
-cache_write_t * search_in_cache_mem1(int fd, off_t offset);
-
+int lab2_open(const char * path);
+int lab2_close(int fd);
+ssize_t lab2_read(int fd, void* buf, size_t count);
+ssize_t lab2_write(int fd,  void * buf, size_t count);
+off_t lab2_lseek(int fd, off_t offset, int whence);
+int lab2_fsync(int fd);
+cache_write_t * search_in_cache_mem(int fd, off_t offset);
 #endif
